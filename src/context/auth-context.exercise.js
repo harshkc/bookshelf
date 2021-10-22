fb0499e7ef094c0d1117dddcf41232dc7c029fc9
harshkc <harsh.hkc1006@gmail.com>
2021-10-22 23:19:08 +0530

    Refactor: colocated Authprovider and app global provider  :tada:

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,80 @@
-// 🐨 create and export a React context variable for the AuthContext
-// 💰 using React.createContext
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
+import * as React from 'react'
+import * as auth from 'auth-provider'
+import {FullPageSpinner, FullPageErrorFallback} from 'components/lib'
+import {client} from 'utils/api-client'
+import {useAsync} from 'utils/hooks'
+
+const AuthContext = React.createContext()
+AuthContext.displayName = 'AuthContext'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function AuthProvider(children) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
+
+  if (isSuccess) {
+    const props = {user, login, register, logout}
+    return <AuthContext.Provider value={props} {...children} />
+  }
+}
+
+function useAuth() {
+  const context = React.useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within a AuthProvider')
+  }
+  return context
+}
+
+function useClient() {
+  const {
+    user: {token},
+  } = useAuth()
+  return React.useCallback(
+    (endpoint, config) => client(endpoint, {...config, token}),
+    [token],
+  )
+}
+
+export {AuthProvider, useAuth, useClient}
