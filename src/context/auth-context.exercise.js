e4248344f9d134b266ee8194ffdab2298e2a2da9
harshkc <harsh.hkc1006@gmail.com>
2021-10-23 22:36:01 +0530

    Performance: adds render as you fetch

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -6,18 +6,26 @@ import * as auth from 'auth-provider'
 import {client} from 'utils/api-client'
 import {useAsync} from 'utils/hooks'
 import {FullPageSpinner, FullPageErrorFallback} from 'components/lib'
+import {queryCache} from 'react-query'
+import {setQueryDataForBook} from 'utils/books'
 
 async function getUser() {
   let user = null
 
   const token = await auth.getToken()
   if (token) {
-    const data = await client('me', {token})
+    const data = await client('bootstrap', {token})
     user = data.user
+    queryCache.setQueryData('list-items', data.listItems, {staleTime: 5000})
+    for (const listItem of data.listItems) {
+      setQueryDataForBook(listItem.book)
+    }
   }
 
   return user
 }
+
+const userPromise = getUser()
 
 const AuthContext = React.createContext()
 AuthContext.displayName = 'AuthContext'
@@ -36,14 +44,6 @@ function AuthProvider(props) {
   } = useAsync()
 
   React.useEffect(() => {
-    // we need to call getUser() sooner.
-    // 🐨 move the next line to just outside the AuthProvider
-    // 🦉 this means that as soon as this module is imported,
-    // it will start requesting the user's data so we don't
-    // have to wait until the app mounts before we kick off
-    // the request.
-    // We're moving from "Fetch on render" to "Render WHILE you fetch"!
-    const userPromise = getUser()
     run(userPromise)
   }, [run])
 
@@ -60,12 +60,10 @@ function AuthProvider(props) {
     setData(null)
   }, [setData])
 
-  const value = React.useMemo(() => ({user, login, logout, register}), [
-    login,
-    logout,
-    register,
-    user,
-  ])
+  const value = React.useMemo(
+    () => ({user, login, logout, register}),
+    [login, logout, register, user],
+  )
 
   if (isLoading || isIdle) {
     return <FullPageSpinner />
